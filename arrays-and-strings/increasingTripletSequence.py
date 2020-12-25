30ab8e36b0b7fc958689953053890a06e1409b32
Azeem Vasanwala <azeem.iron@gmail.com>
2020-12-25 19:42:13 +0800

    Add notes

--- arrays-and-strings/increasingTripletSequence.py
+++ arrays-and-strings/increasingTripletSequence.py
@@ -1,3 +1,10 @@
+# Notes:
+# I did not fully understand this question when I did it.
+# Conditions are such that i, j and k do not need to be consecutive.
+# Idea: Lock in i and j first as soon as you can.
+# If you find a number greater than i, either that or your current j can be the scond number
+# Finding k is a matter of finding the first number greater than j.
+
 class Solution(object):
     def increasingTriplet(self, nums):
         """
@@ -15,7 +22,7 @@ class Solution(object):
                 return True
         return False
             
-# print(Solution().increasingTriplet([1,2,3,4,5]))
-# print(Solution().increasingTriplet([5,4,3,2,1]))
-# print(Solution().increasingTriplet([2,1,5,0,4,6]))
+print(Solution().increasingTriplet([1,2,3,4,5]))
+print(Solution().increasingTriplet([5,4,3,2,1]))
+print(Solution().increasingTriplet([2,1,5,0,4,6]))
 print(Solution().increasingTriplet([20,100,10,12,5,13]))
